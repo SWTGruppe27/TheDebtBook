3b02d57ad2cefa13ea089de6cf1e2d282795ec8c
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-03-08 11:01:21 +0100

    added ting

--- TheDebtBook/Views/AddDebtor.xaml.cs
+++ TheDebtBook/Views/AddDebtor.xaml.cs
@@ -10,14 +10,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using TheDebtBook.Models;
+using TheDebtBook.ViewModels;
 
 namespace TheDebtBook.Views
 {
-    /// <summary>
-    /// Interaction logic for AddDebtor.xaml
-    /// </summary>
     public partial class AddDebtor : Window
     {
+        private DebtBookViewModel debtBookViewModel;
         private Debtor _addDebtor;
         public AddDebtor()
         {
